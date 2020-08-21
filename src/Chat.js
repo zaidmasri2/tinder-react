c49d9c29c8188ebcc8b065db7b9edf46fe0fcf99
zaidmasri2 <zaidmasri2@gmail.com>
2020-08-21 02:51:05 -0400

    Importing Chat comp in Chats

---
+++ src/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+
+function Chat({ name, message, profilePic, timestamp }) {
+  return (
+    <div className="chat">
+      <Avatar className="chat__image" alt={name} src={profilePic} />;
+    </div>
+  );
+}
+
+export default Chat;
--- src/Chats.js
+++ src/Chats.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Chats.css";
+import Chat from "./Chat";
 
 function Chats() {
-  return <div></div>;
+  return (
+    <div className="chats">
+      <Chat
+        name="Mark"
+        message="Yo what's up!"
+        timestamp="40 seconds ago"
+        profilePic=""
+      />
+    </div>
+  );
 }
 
 export default Chats;
