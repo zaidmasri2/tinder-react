a276141b193e08b432664a3bc197c4e8f55b5235
zaidmasri2 <zaidmasri2@gmail.com>
2020-08-21 03:34:40 -0400

    Made things look nice

--- src/Chats.js
+++ src/Chats.js
@@ -6,10 +6,22 @@ function Chats() {
   return (
     <div className="chats">
       <Chat
-        name="Mark"
+        name="Sara"
         message="Yo what's up!"
         timestamp="40 seconds ago"
-        profilePic=""
+        profilePic="https://image.freepik.com/free-vector/smiling-girl-avatar_102172-32.jpg"
+      />
+      <Chat
+        name="Emily"
+        message="No way."
+        timestamp="2 Hours Ago"
+        profilePic="https://thumbs.dreamstime.com/b/cute-girl-avatar-icon-young-woman-glasses-pretty-lady-portrait-cartoon-illustration-beautiful-brunette-76317721.jpg"
+      />
+      <Chat
+        name="Lex"
+        message="See you then"
+        timestamp="7 hours ago"
+        profilePic="https://thumbs.dreamstime.com/b/girl-lovely-avatar-icon-blonde-hair-cartoon-91929591.jpg"
       />
     </div>
   );
